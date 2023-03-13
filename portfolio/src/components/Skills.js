0493a27b9e478174d1d92413f520b30e142a74f4
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-13 13:34:24 +0000

    skills component added

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -3,11 +3,12 @@ import "./SkillsStyles.css";
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3, faReact, faSquareJs, faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import { faVialCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Skills = () => {
     return (
         <div className="bottom">
-            <h1>My Skills</h1>
+            <h1>Engineering Skills</h1>
             <div className="skills">
                 <div className="html">
                     <FontAwesomeIcon className="icons" icon={faHtml5} />
@@ -15,28 +16,34 @@ const Skills = () => {
                         <p>HTML</p>
                     </div>
                 </div>
-                <div className="html">
+                <div className="css">
                     <FontAwesomeIcon className="icons" icon={faCss3} />
                     <div>
                         <p>CSS</p>
                     </div>
                 </div>
-                <div className="html">
+                <div className="js">
                     <FontAwesomeIcon className="icons" icon={faSquareJs} />
                     <div>
                         <p>JavaScript</p>
                     </div>
                 </div>
-                <div className="html">
+                <div className="react">
                     <FontAwesomeIcon className="icons" icon={faReact} />
                     <div>
                         <p>React</p>
                     </div>
                 </div>
-                <div className="html">
+                <div className="node">
                     <FontAwesomeIcon className="icons" icon={faNodeJs} />
                     <div>
                         <p>Node.js</p>
+                    </div>
+                </div>
+                <div className="testing">
+                    <FontAwesomeIcon className="icons" icon={faVialCircleCheck} />
+                    <div>
+                        <p>Testing</p>
                     </div>
                 </div>
             </div>
