0cefe282eec5acb87619f86719dba5904b3b527b
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-13 20:22:38 +0000

    about me layout

--- portfolio/src/components/AboutContent.js
+++ portfolio/src/components/AboutContent.js
@@ -6,20 +6,21 @@ import MyCv from "../assets/Amiker Best Resume 2023.pdf";
 
 const AboutContent = () => {
     return (
-        <div className="about">
-            <div className="left">
-                <h1>Amiker Best</h1>
-                <p>A bit about me</p>
-                <a href={MyCv} target="_blank" rel="noopener noreferrer">
-                    <button className="btn-light cv-btn">CV</button>
-                </a>
-            </div>
-            <div className="right">
-                <div className="img-container">
-                    <img src={Me} className="img" alt="true" />
+        <div className="dotted-line">
+            <div className="about">
+                <div className="left">
+                    <h1>Amiker Best</h1>
+                    <p>I am an innovative and driven individual with a proven track record in software engineering, with a particular focus on frontend development. I have successfully delivered diverse web applications as both an individual contributor and a team player, I am constantly seeking opportunities to improve my skills. I am seeking a junior frontend engineering role within the FinTech industry, with a company that offers excellent training opportunities and a clear path for career progression.</p>
+                    <a href={MyCv} target="_blank" rel="noopener noreferrer">
+                        <button className="btn">CV</button>
+                    </a>
+                </div>
+                <div className="right">
+                    <div className="img-container">
+                        <img src={Me} className="img" alt="true" />
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
