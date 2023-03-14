ca39db7ee0094378fe7aeec61249dd4f3958e282
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-03-14 12:44:17 +0000

    footer style changed

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,50 +4,37 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLocationDot, faMobile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-container ">
-
-                {/* <div className="navTop">
-                    <div className="social">
-                        <a href="https://www.linkedin.com/in/amikerbest/">
-                            <FontAwesomeIcon className="icons" icon={faLinkedin} />
-                        </a>
-                        <a href="https://github.com/AmikerB">
-                            <FontAwesomeIcon className="icons" icon={faGithub} />
-                        </a>
-                        <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Famikerbest%2F%3Ffbclid%3DIwAR1KCKLmRt9wzF7tIwFX8Q4XNBYTVWiaFXKOgyFpjeVe8hqQZeS191M43cE&h=AT2GFFnMkS9a86U6lavq1pPABchYI7ce7rYYoEsdnCoymjEKuTJdKGwWw63hMGZ-4Ylpfzl-pavMfDJsVvt-7Z5Aup3QzN0ScHcX41Dna_TvtY4o_sa47Kl7vxTeKwVTvvFV5omNgYfUBagyexqHxA">
-                            <FontAwesomeIcon className="icons" icon={faInstagram} />
-                        </a>
-                    </div>
-                </div> */}
-                <div className="navBottom">
-                    <div className="location">
-                        <FontAwesomeIcon className="icons" icon={faLocationDot} />
-                        <div>
-                            <p>London</p>
-                        </div>
-                    </div>
-                    <div className="email">
-                        <a href="mailto:best.amiker@gmail.com" rel="noreferrer" target="_blank"><FontAwesomeIcon className="icons" icon={faEnvelope} />
-                            <div>
-                                <p>best.amiker@gmail.com</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="phone">
-                        <a href="tel:07738398290">
-                            <FontAwesomeIcon className="icons" icon={faMobile} />
-                            <div>
-                                <p>+44 7738398290</p>
-                            </div>
-                        </a>
-                    </div>
-
+            <div className="footer-top ">
+                <div className="footer-left">
+                    <h2><FontAwesomeIcon className="icons" icon={faLocationDot} />Based In</h2>
+                    <p className="location">London, UK</p>
                 </div>
-
+                <div className="footer-middle">
+                    <h2><FontAwesomeIcon className="icons" icon={faEnvelope} />Email</h2>
+                    <a className="email" href="mailto:best.amiker@gmail.com" rel="noreferrer" target="_blank">best.amiker@gmail.com</a>
+                </div>
+                <div className="footer-middle">
+                    <h2><FontAwesomeIcon className="icons" icon={faMobile} />Phone</h2>
+                    <a className="phone" href="tel:07738398290">+44 7738398290</a>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <div className="social">
+                    <a href="https://www.linkedin.com/in/amikerbest/" rel="noreferrer" target="_blank">
+                        <FontAwesomeIcon className="icons" icon={faLinkedin} />
+                    </a>
+                    <a href="https://github.com/AmikerB" rel="noreferrer" target="_blank">
+                        <FontAwesomeIcon className="icons" icon={faGithub} />
+                    </a>
+                    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Famikerbest%2F%3Ffbclid%3DIwAR1KCKLmRt9wzF7tIwFX8Q4XNBYTVWiaFXKOgyFpjeVe8hqQZeS191M43cE&h=AT2GFFnMkS9a86U6lavq1pPABchYI7ce7rYYoEsdnCoymjEKuTJdKGwWw63hMGZ-4Ylpfzl-pavMfDJsVvt-7Z5Aup3QzN0ScHcX41Dna_TvtY4o_sa47Kl7vxTeKwVTvvFV5omNgYfUBagyexqHxA" rel="noreferrer" target="_blank">
+                        <FontAwesomeIcon className="icons" icon={faInstagram} />
+                    </a>
+                </div>
             </div>
         </div>
     )
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../index.css";
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import Hero2 from "../components/Hero2";
@@ -12,13 +13,7 @@ const Projects = () => {
             <Navbar />
             <Wrapper>
                 <Hero2 heading="PROJECTS" text="Check out some of my work" />
-                <div style={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(3, 1fr)",
-                    gap: "10px",
-                    marginTop: "10vh",
-                    padding: "10px"
-                }}>
+                <div className="workContainer">
                     {projectData.map((project) => (
                         <ProjectCards key={project.name} name={project.name} image={project.image} details={project.details} pageLink={project.pageLink} pageSource={project.pageSource} />
                     ))}
